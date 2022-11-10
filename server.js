f3a92752095604d118cbf1521c34f74d39126ab3
Melika Hamedani <97775710+Melikahamedani@users.noreply.github.com>
2022-11-10 15:10:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-//Cyclic url: //Cyclic url: https://gray-abalone-hat.cyclic.app/
-
-
+//Cyclic url: https://gray-abalone-hat.cyclic.app/
 var express = require("express");
 var app = express();
 var data_prep = require("./data_prep.js");
 var path = require("path");
-const exphbs = require("express-handlebars");
+const {engine} = require("express-handlebars");
+
 
 
 app.use(express.json());
@@ -15,11 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 var HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
+
     console.log("Express http server listening on " + HTTP_PORT);
+
 }
 
 //Tell server to use handlebars
-app.engine(".hbs", exphbs.engine({
+app.engine(".hbs", engine({
     extname:".hbs" ,
     defaultLayout: "main"
 }));
@@ -29,7 +30,7 @@ app.set("view engine", ".hbs");
 
 //Route
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render('home')
 });
 
 
@@ -37,52 +38,58 @@ app.get("/", (req, res) => {
 //CPA Students
 app.get("/CPA", (req, res) => {
     data_prep.cpa().then((data) => {
-        res.render("students", { students: data })
+        res.render('students', { students: data })
     }).catch((reason) => {
         res.json({ message: reason });
     });
 
 });
 
-
 //All Students
 app.get("/allStudents", (req, res) => {
     data_prep.allStudents().then((data) => {
-        res.render("students", { students: data })
+        res.render('students', { students: data })
     }).catch((reason) => res.json({ message: reason }));
 });
-
 
 //Add a new student
 app.get("/addStudent", (req, res) => {
-    res.render("addStudent");
+    res.render('addStudent')
 });
+
 app.post("/addStudent", (req, res) => {
     data_prep.addStudent(req.body).then(() => {
         var data = req.body;
-        res.render("addStudent", { data })
+        res.render('student', { data })
     }).catch((reason) => res.json({ message: reason }));
-
 });
 
 
 //Highest GPA
 app.get("/highGPA", (req, res) => {
     data_prep.highGPA().then((data) => {
-        res.render("student", { data })
+        res.render('student', { data })
     });
 });
 
 
 //Error
-app.get("*", (req, res)=>{
+app.get("*", (req, res) => {
+
     res.status(404).send("Error 404: page not found.")
+
 });
 
 
-//intitization
-data_prep.prep().then((data)=>{
+
+data_prep.prep().then((data) => {
+
+    //console.log(data);
+
     app.listen(HTTP_PORT, onHttpStart);
-}).catch((err)=>{
+
+}).catch((err) => {
+
     console.log(err);
+
 });
